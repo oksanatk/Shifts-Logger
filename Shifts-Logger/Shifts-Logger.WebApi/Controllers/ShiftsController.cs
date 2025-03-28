39f33e2c893b5bc502115c14dee4f238bf97c41f
oksanatk <tkachoksn@gmail.com>
2025-03-28 12:53:53 -0400

    file-scoped namespace

--- Shifts-Logger/Shifts-Logger.WebApi/Controllers/ShiftsController.cs
+++ Shifts-Logger/Shifts-Logger.WebApi/Controllers/ShiftsController.cs
@@ -2,42 +2,41 @@
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace Shifts_Logger.WebApi.Controllers
+namespace Shifts_Logger.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ShiftsController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ShiftsController : ControllerBase
+    // GET: api/Shifts
+    [HttpGet]
+    public IEnumerable<string> Get()
     {
-        // GET: api/Shifts
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        return new string[] { "value1", "value2" };
+    }
 
-        // GET api/Shifts/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return $"value {id}";
-        }
+    // GET api/Shifts/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return $"value {id}";
+    }
 
-        // POST api/Shifts
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+    // POST api/Shifts
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
 
-        // PUT api/Shifts/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+    // PUT api/Shifts/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
 
-        // DELETE api/Shifts/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+    // DELETE api/Shifts/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
     }
 }
--- Shifts-Logger/Shifts-Logger.WebApi/Models/Worker.cs
+++ Shifts-Logger/Shifts-Logger.WebApi/Models/Worker.cs
@@ -1,9 +1,8 @@
-﻿namespace Shifts_Logger.WebApi.Models
+﻿namespace Shifts_Logger.WebApi.Models;
+
+public class Worker
 {
-    public class Worker
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = null!;
-        public Shift[] Shifts { get; set; } = null!;
-    }
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public Shift[] Shifts { get; set; } = null!;
 }
