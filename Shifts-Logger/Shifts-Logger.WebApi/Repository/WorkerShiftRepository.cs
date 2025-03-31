ebf824e48d1884ccdbeaaa1ab65937c41e5d10fa
oksanatk <tkachoksn@gmail.com>
2025-03-31 15:47:58 -0400

    crud operations for repo complete, untested.

--- Shifts-Logger/Shifts-Logger.WebApi/Repository/WorkerShiftRepository.cs
+++ Shifts-Logger/Shifts-Logger.WebApi/Repository/WorkerShiftRepository.cs
@@ -19,4 +19,71 @@ public class WorkerShiftRepository
 
         return allShifts;
     }
+
+    public async Task<List<Shift>> ReadAllShiftsForWorker(Worker worker)
+    {
+        List<Shift> workerShifts =  await _context.Shifts.Where(s => s.WorkerId == worker.Id).ToListAsync();
+
+        return workerShifts;
+    }
+
+    public async Task AddWorker(string name)
+    {
+        await _context.Workers.AddAsync(new Worker { Name = name });
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task AddShiftToWorker(int workerId, DateTime startTime, DateTime endTime)
+    {
+        await _context.Shifts.AddAsync(new Shift
+        {
+            WorkerId = workerId,
+            StartTime = startTime,
+            EndTime = endTime
+        });
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateWorker(int id, string newName)
+    {
+        Worker? worker = await _context.Workers.SingleOrDefaultAsync(w => w.Id == id);
+        if (worker != null)
+        {
+            worker.Name = newName;
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task UpdateShift(int id, DateTime newStartTime, DateTime newEndTime)
+    {
+        Shift? shift = await _context.Shifts.SingleOrDefaultAsync(s => s.Id == id);
+        if (shift != null)
+        {
+            shift.StartTime = newStartTime;
+            shift.EndTime = newEndTime;
+            await _context.SaveChangesAsync();                                           
+        }
+    }
+
+    public async Task DeleteWorker(int id)
+    {
+        Worker? worker = await _context.Workers.SingleOrDefaultAsync(w => w.Id == id);
+
+        if (worker != null)
+        {
+            _context.Workers.Remove(worker);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task DeleteShift(int id)
+    {
+        Shift? shift = await _context.Shifts.SingleOrDefaultAsync(s => s.Id == id);
+        if (shift != null)
+        {
+            _context.Shifts.Remove(shift);
+            await _context.SaveChangesAsync();
+        }
+    }
+
 }
